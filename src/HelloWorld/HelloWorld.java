3296e83ad438ac482078bebb233a753852864246
GuiLS <lisen_work@163.com>
2018-11-28 01:01:17 +0800

    从远程提交git

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -3,6 +3,7 @@ package HelloWorld;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello, world");
+        System.out.println("提交git");
     }
 }
 
