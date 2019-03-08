94aa6928c6e8ec6e6bcf7dbc40775f99048ff841
GuiLS <lisen_work@163.com>
2019-03-08 23:19:55 +0800

    work for chapter1_2

---
+++ src/com/lalala/algorithms/chapter1_2/SmallDate.java
@@ -0,0 +1,45 @@
+package com.lalala.algorithms.chapter1_2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * @Auther: lisen
+ * @Date: 2019/3/8 22:43
+ * @Description:
+ */
+public class SmallDate {
+    private final int value;
+
+    public SmallDate(int m, int d, int y) {
+        value = y * 512 + m * 32 + d;
+        // 简单猜测计算了一下，输入条件应该限定在：m 取值范围在[0,15] (2^4-1)， d 取值范围在[0,31] (2^5-1).
+    }
+
+    public int month() {
+        return (value / 32) % 16;
+        // 除以32后，剩下 y*2^4 + m（d消掉了，2^4 = 2^9 / 2^5），接着再用16(即2^4)取余,得m
+    }
+
+    public int day() {
+        return value % 32;
+        // 用32取余自然剩下余数d
+    }
+
+    public int year() {
+        return value / 512;
+        // 用512除法自然得到y
+    }
+
+    @Override
+    public String toString() {
+        return month() + "/" + day() + "/" + year();
+    }
+
+    public static void main(String[] args) {
+        int m = 10;
+        int d = 31;
+        int y = 2012;
+        SmallDate date = new SmallDate(m, d, y);
+        StdOut.println(date);
+    }
+}
