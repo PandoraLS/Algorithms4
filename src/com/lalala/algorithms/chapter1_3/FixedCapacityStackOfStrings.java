66ab7af961752b44eb8386f6ed81a5755956e8bb
PandoraLS <lisen_work@163.com>
2019-03-18 23:15:00 +0800

    work for chapter1_3

--- src/com/lalala/algorithms/chapter1_3/FixedCapacityStackOfStrings.java
+++ src/com/lalala/algorithms/chapter1_3/FixedCapacityStackOfStrings.java
@@ -37,10 +37,10 @@ public class FixedCapacityStackOfStrings {
         s = new FixedCapacityStackOfStrings(100);
 
         while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
+            String item = StdIn.readString(); //读取字符串的操作并不会读取空格,只是读取字符串
             if (!item.equals("-"))
                 s.push(item);
-            else if (!s.isEmpty()) StdOut.print(s.pop() + "  ");
+            else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
         }
         System.out.println("(" + s.size() + " left on stack)");
     }
